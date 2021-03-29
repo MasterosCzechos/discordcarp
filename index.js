e9a0efaf5d3512d09bd6532fed15180cad4588de
MasterosCzechos <kusy1506@gmail.com>
2021-03-29 19:52:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on('message', async message => {
         }
     }
     //REACTION ON BAD WORD
-    bad = ['piča', 'kokot', 'píča','debil','fuck','fuk','f*ck','pí*a','pi*a','kretén','kreten','pičus','pixi'];
+    bad = ['piča', 'kokot', 'píča','debil','fuck','fuk','f*ck','pí*a','pi*a','kretén','kreten','pičus','pixi','mrdka','kunda','kurva','čůrák','čurak','curak'];
     if (bad.indexOf(message.content) >= 0) {
         message.react('🤬');
     }
