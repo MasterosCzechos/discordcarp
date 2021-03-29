33cf54beb59edcc08f55f7f3eaf47d7a88d6f22f
MasterosCzechos <kusy1506@gmail.com>
2021-03-29 19:32:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('message', async message => {
     //REACTION ON BAD WORD
     bad = ['piča', 'kokot'];
     if (bad.indexOf(message.content) >= 0) {
-        message.react('826140093601415208');
+        message.react(':rage:');
     }
 //CARP
 //KOSTKA
