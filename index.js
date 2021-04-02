99e938e3f0259a738b7806ae26bba5b80aa59e23
MasterosCzechos <kusy1506@gmail.com>
2021-04-02 20:09:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', async message => {
     bad = ['píči','piča', 'kokot', 'píča','debil','fuck','fuk','f*ck','pí*a','pi*a','kretén','kreten','pičus','pixi','mrdka','kunda','kurva','čůrák','čurak','curak'];
     if (bad.indexOf(message.content) >= 0) {
         message.react('🤬');
-        message.react('827604741118623824');
+        message.react('827604658729517076');
     }
     if (message.content.includes ('gay')){
         message.channel.send(`Sám jsi ;)`);
